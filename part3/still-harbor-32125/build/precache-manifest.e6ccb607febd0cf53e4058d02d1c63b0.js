9d78eeb71f734d092a5ed1bc2d58ba9b0ae5525f
Alchemistst <sisesilbar@gmail.com>
2019-12-09 11:05:27 +0100

    Added part3 / Ready for deployment on Heroku

---
+++ part3/still-harbor-32125/build/precache-manifest.e6ccb607febd0cf53e4058d02d1c63b0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6621b945e653fd4f9988aeee7ae4e739",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d381ccf4c3aa730bfa9",
+    "url": "/static/js/2.2b16c874.chunk.js"
+  },
+  {
+    "revision": "dc4bd3da648d83897fb3",
+    "url": "/static/js/main.184a96d0.chunk.js"
+  },
+  {
+    "revision": "df99deec2933845bfe71",
+    "url": "/static/js/runtime-main.b4276010.js"
+  }
+]);
